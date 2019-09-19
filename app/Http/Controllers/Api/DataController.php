4328c16e2de7296b401be54e178fca957c505655
swrdism <swrdism@gamil.com>
2019-09-19 14:51:33 +0800

    Set api that can revice data in route/api.php

---
+++ app/Http/Controllers/Api/DataController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Services\DataService;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DataController extends Controller
+{
+    public function getStoreDataList(){
+        return (new DataService())->getStoreDataList();
+    }
+    public function getIsUpdating(){
+        return (new DataService())->getIsUpdating();
+    }
+
+    public function getIsUpdated(){
+        return (new DataService())->getIsUpdated();
+    }
+
+    public function getClassNameDataList(){
+        return (new dataService())->getClassNameDataList();
+    }
+    public function getTeacherDataList()
+    {
+        return (new dataService())->getTeacherDataList();
+    }
+}
